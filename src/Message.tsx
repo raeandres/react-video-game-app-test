e865279e29023fdcd9682c620b81ebb6561e00ba
Rae-An Andres <raeandres@users.noreply.github.com>
2025-01-26 14:20:11 +0800

    Added simple message component
    Simple message component for quickly explaining the basic functions of react

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+
+// PascalCasing
+function Message() {
+    // JSX: Javascript XML
+    const name = 'Rae-An';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    else
+        return <h1>Hello there</h1>
+}
+
+export default Message;
