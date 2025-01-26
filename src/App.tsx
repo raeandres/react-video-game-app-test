34625f4116b1c86168a14a52584b0b2317455cb7
Rae-An Andres <raeandres@users.noreply.github.com>
2025-01-26 15:58:56 +0800

    used hook (useState)
    Used (useState) hook to store and handle event; used this to display and show the color of the clicked item in the list

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,12 @@
 import ListGroup from "./components/ListGroup";
-import { MouseEvent } from "react";
 
 // import Message from "./Message";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const message = items.length === 0 ? <p>No Item found</p> : null;
-
-  const getMessage = () => {
-    return items.length === 0 ? <p>No Item found</p> : null;
-  };
-
-  const getMessageShorter = () => {
-    return items.length === 0 && <p>No Item found</p>; // more concise way
-  };
-
-  // Event Handling
-  const handleClick = (event: MouseEvent) => console.log(event);
-
   return (
-    <>
-      <h1>List</h1>
-      {getMessageShorter()}
-      <ul className="list-group">
-        {items.map((item, idx) => (
-          <li
-            className="list-group-item active"
-            key={idx}
-            onClick={handleClick}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
-    </>
+    <div>
+      <ListGroup />
+    </div>
   );
 }
 
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,51 @@
+import { MouseEvent, useState } from "react";
+
 function ListGroup() {
-    return <h1>List Group</h1>;
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const message = items.length === 0 ? <p>No Item found</p> : null;
+
+  const getMessage = () => {
+    return items.length === 0 ? <p>No Item found</p> : null;
+  };
+
+  const getMessageShorter = () => {
+    return items.length === 0 && <p>No Item found</p>; // more concise way
+  };
+
+  // hook
+  const state = useState(-1);
+  state[0]; // variable (selectedIndex)
+  state[1]; // updated function
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // Event Handling
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>List</h1>
+      {getMessageShorter()}
+      <ul className="list-group">
+        {items.map((item, idx) => (
+          <li
+            className={
+              selectedIndex == idx
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={idx}
+            onClick={() => {
+              setSelectedIndex(idx);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
