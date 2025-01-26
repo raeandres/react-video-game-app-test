6dbea6c54f840615c7930de9ca4dbd0f9670eeb4
Rae-An Andres <raeandres@users.noreply.github.com>
2025-01-26 15:12:11 +0800

    Conditional Statements
    Early return

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,24 @@ import ListGroup from "./components/ListGroup";
 // import Message from "./Message";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No Item Found</p>
+      </>
+    );
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item, idx) => (
+          <li key={idx}>{item}</li>
+        ))}
       </ul>
     </>
   );
